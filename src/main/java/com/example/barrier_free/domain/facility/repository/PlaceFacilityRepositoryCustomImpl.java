e00bb9fdf2c686093781145911644f428df5048b
qlcsk <qlcskcode@naver.com>
2025-06-26 23:09:00 +0900

    feat: 좋아요 조회와 편의시설 필터링을 위한 Repository 로직 추가

---
+++ src/main/java/com/example/barrier_free/domain/facility/repository/PlaceFacilityRepositoryCustomImpl.java
@@ -0,0 +1,49 @@
+package com.example.barrier_free.domain.facility.repository;
+
+import static com.example.barrier_free.domain.facility.entity.QMapFacility.*;
+import static com.example.barrier_free.domain.facility.entity.QReportFacility.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.barrier_free.domain.map.entity.Map;
+import com.example.barrier_free.domain.map.entity.QMap;
+import com.example.barrier_free.domain.report.entity.QReport;
+import com.example.barrier_free.domain.report.entity.Report;
+import com.example.barrier_free.global.common.Place;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class PlaceFacilityRepositoryCustomImpl implements PlaceFacilityRepositoryCustom {
+
+	private final JPAQueryFactory queryFactory;
+	private static final QMap map = QMap.map;
+	private static final QReport report = QReport.report;
+
+	@Override
+	public List<Place> findPlacesHavingAllFacilities(List<Integer> facilityIds) {
+
+		List<Map> maps = queryFactory
+			.select(mapFacility.map)
+			.from(mapFacility)
+			.where(mapFacility.facility.id.in(facilityIds))
+			.groupBy(mapFacility.map)
+			.having(mapFacility.facility.id.countDistinct().eq((long)facilityIds.size()))
+			.fetch();
+
+		List<Report> reports = queryFactory
+			.select(reportFacility.report)
+			.from(reportFacility)
+			.where(reportFacility.facility.id.in(facilityIds))
+			.groupBy(reportFacility.report)
+			.having(reportFacility.facility.id.countDistinct().eq((long)facilityIds.size()))
+			.fetch();
+		List<Place> p = new ArrayList<>();
+		p.add((Place)maps);
+		p.add((Place)reports);
+		return p;
+	}
+
+}
---
+++ src/main/java/com/example/barrier_free/domain/favorite/repository/FavoriteRepositoryCustom.java
@@ -0,0 +1,11 @@
+package com.example.barrier_free.domain.favorite.repository;
+
+import java.util.List;
+
+import com.example.barrier_free.domain.favorite.dto.FavoritePlaceGroup;
+
+public interface FavoriteRepositoryCustom {
+	FavoritePlaceGroup findFilteredFavorites(Long userId, List<Integer> facilityIds);
+
+	FavoritePlaceGroup findFavoriteByUserIdWithPlace(Long userId);
+}
