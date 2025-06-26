8a6f9b37bee7a0324e8f40ee74271dc7c988d3a1
qlcsk <qlcskcode@naver.com>
2025-06-26 23:13:13 +0900

    refactor: Map/Report 속성 추출을 위한 Place 인터페이스 메서드 정의

--- src/main/java/com/example/barrier_free/domain/map/entity/Map.java
+++ src/main/java/com/example/barrier_free/domain/map/entity/Map.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 import com.example.barrier_free.domain.facility.entity.MapFacility;
 import com.example.barrier_free.domain.favorite.entity.Favorite;
+import com.example.barrier_free.domain.favorite.entity.MonthlyRank;
 import com.example.barrier_free.domain.review.entity.Review;
 import com.example.barrier_free.global.common.Place;
 import com.example.barrier_free.global.common.PlaceEntity;
+import com.example.barrier_free.global.common.PlaceType;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
@@ -15,7 +17,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Map extends PlaceEntity implements Place {
 	@Id
@@ -31,10 +36,35 @@ public class Map extends PlaceEntity implements Place {
 	private List<Favorite> favorites = new ArrayList<>();
 	@OneToMany(mappedBy = "map", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<Review> reviews = new ArrayList<>();
+	@OneToMany(mappedBy = "map", cascade = CascadeType.REMOVE, orphanRemoval = true)
+	private List<MonthlyRank> monthlyRanks = new ArrayList<>();
 
 	@Override
 	public void attachTo(Review review) {
 		review.attachMap(this); // 리뷰에 맵 연결
+	}
+
+	@Override
+	public Long getId() {
+		return id;
+	}
+
+	@Override
+	public String getDescription() {
+		return "";
+	}
+
+	@Override
+	public PlaceType getPlaceType() {
+		return PlaceType.map;
+	}
+
+	@Override
+	//lazy로딩이므로 transactional 안에서만 사용
+	public List<Integer> getFacility() {
+		return mapFacilities.stream()
+			.map(mapFacility -> mapFacility.getFacility().getId())
+			.toList();
 	}
 
 }
