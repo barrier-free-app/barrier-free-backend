68d30dafd69224ff8cb0f091f8e04d40d9af7c41
qlcsk <qlcskcode@naver.com>
2025-07-04 14:12:20 +0900

    chore: 사용하지 않는 리뷰 커스텀 리포지토리 및 구현체 삭제

--- src/main/java/com/example/barrier_free/domain/review/repository/ReviewRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.example.barrier_free.domain.review.repository;
-
-import java.util.List;
-
-import com.example.barrier_free.domain.review.entity.Review;
-
-public interface ReviewRepositoryCustom {
-	List<Review> findAllWithDetailsByUserId(Long userId);
-}
