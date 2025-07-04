a44795e55954832d0988f67c50787d31d9f41ad6
qlcsk <qlcskcode@naver.com>
2025-07-04 13:34:31 +0900

    feat: 리뷰 조회용 Repository 메서드 추가 (EntityGraph 적용)

--- src/main/java/com/example/barrier_free/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/example/barrier_free/domain/review/repository/ReviewRepository.java
@@ -1,7 +1,5 @@
 package com.example.barrier_free.domain.review.repository;
 
-import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
@@ -16,5 +14,6 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
 	@EntityGraph(attributePaths = {"user"})
 	Page<Review> findByMapId(Long id, Pageable pageable);
 
-	List<Review> findByUserId(Long userId);
+	@EntityGraph(attributePaths = {"map", "report", "user"})
+	Page<Review> findByUserId(Long userId, Pageable pageable);
 }
