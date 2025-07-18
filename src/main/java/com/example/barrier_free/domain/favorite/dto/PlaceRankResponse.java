83abe528adff44477fd9bc027e3fc6a9577e8700
qlcsk <qlcskcode@naver.com>
2025-07-18 16:45:18 +0900

    feat: 좋아요 조회와 랭킹 조회시의 dto 분리

---
+++ src/main/java/com/example/barrier_free/domain/favorite/dto/PlaceRankResponse.java
@@ -0,0 +1,54 @@
+package com.example.barrier_free.domain.favorite.dto;
+
+import java.util.List;
+
+import com.example.barrier_free.domain.favorite.entity.WeeklyRank;
+import com.example.barrier_free.domain.map.entity.Map;
+import com.example.barrier_free.domain.report.entity.Report;
+import com.example.barrier_free.global.common.PlaceType;
+import com.example.barrier_free.global.exception.CustomException;
+import com.example.barrier_free.global.response.ErrorCode;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class PlaceRankResponse {
+	private Long placeId;
+	private PlaceType placeType;
+	private String name;
+	private String region;
+	private String description;
+	private List<Integer> facility;
+	private int imageType;
+
+	public static PlaceRankResponse from(WeeklyRank weeklyRank) {
+		if (weeklyRank.getMap() != null) {
+			Map map = weeklyRank.getMap();
+			return new PlaceRankResponse(
+				map.getId(),
+				PlaceType.map,
+				map.getName(),
+				map.getRegion(),
+				map.getDescription(),
+				map.getFacility(),
+				map.getImageType()
+			);
+		} else if (weeklyRank.getReport() != null) {
+			Report report = weeklyRank.getReport();
+			return new PlaceRankResponse(
+				report.getId(),
+				PlaceType.report,
+				report.getName(),
+				report.getRegion(),
+				report.getDescription(),
+				report.getFacility(),
+				report.getImageType()
+			);
+		} else {
+			throw new CustomException(ErrorCode.INVALID_WEEKLY_RANK);
+		}
+
+	}
+}
